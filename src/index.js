86af59afa39c6f5f5dd88074a013df6187504353
guilhermehccruz <guihccruz60@gmail.com>
2022-06-02 16:25:01 -0300

    Challenge completed

--- src/index.js
+++ src/index.js
@@ -10,19 +10,75 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+	const { username } = request.headers;
+
+	if (!username) {
+		return response.status(401).json({error: 'Missing username header'});
+	}
+
+	const user = users.find((user) => user.username === username);
+
+	if (!user) {
+		return response.status(404).json({error: 'User not found'});
+	}
+
+	request.user = user;
+
+	return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+	const { user } = request;
+
+	if (!user || !user.pro && user.todos.length >= 10) {
+		return response.status(403).json({error: "You have reached the limit of 10 todos in the free subscription"});
+	}
+
+	return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+	const { username } = request.headers;
+	const { id } = request.params;
+
+	if (!username) {
+		return response.status(401).json({error: 'Missing username header'});
+	}
+
+	if (!validate(id)) {
+		return response.status(400).json({error: 'Id is invalid'});
+	}
+
+	const user = users.find((user) => user.username === username);
+
+	if (!user) {
+		return response.status(404).json({error: 'User not found'});
+	}
+
+	const todo = user.todos.find((todo) => todo.id === id);
+
+	if (!todo) {
+		return response.status(404).json({error: "Todo not found"});
+	}
+
+	request.user = user;
+	request.todo = todo;
+
+	return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+	const { id } = request.params;
+
+	const user = users.find((user) => user.id === id);
+
+	if (!user) {
+		return response.status(404).json({error: 'User not found'});
+	}
+
+	request.user = user;
+
+	return next();
 }
 
 app.post('/users', (request, response) => {
